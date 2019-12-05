1e51a282cad77d6a587c079b8419ecf549477022
Chris Michael <chris.perez@upr.edu>
2019-12-05 15:18:45 -0400

    Basic eslint configuration.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "prettier"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {}
+};
